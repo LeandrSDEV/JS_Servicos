086888fb00c8d05a8dce421c399feb84173c1df2
LEANDRO <crazyg4ng@gmail.com>
2024-09-20 18:58:27 -0300

    Iniciando JS_Serviços

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JS_Serviços.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

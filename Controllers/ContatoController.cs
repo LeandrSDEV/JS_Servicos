62675e87d4a8cc24cf44c2d81e1049cd31fbcad4
LEANDRO <crazyg4ng@gmail.com>
2024-09-20 22:39:52 -0300

    Interação de Apagar e alterar contato

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -20,13 +20,22 @@ namespace JS_Serviços.Controllers
         {
             return View();
         }
-        public IActionResult Editar()
+        public IActionResult Editar(int id)
         {
-            return View();
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
         }
-        public IActionResult ApagarConfirmacao()
+        public IActionResult ApagarConfirmacao(int id)
         {
-            return View();
+
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+
+        public IActionResult Apagar(int id)
+        {
+            _contatoRepositorio.Apagar(id);
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -35,5 +44,14 @@ namespace JS_Serviços.Controllers
             _contatoRepositorio.Adicionar(contato);
             return RedirectToAction("Index");
         }
+
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+            _contatoRepositorio.Atualizar(contato);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
